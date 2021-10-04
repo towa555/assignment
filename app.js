c9ebfd1b6844bc359ef7cfbfaf18bbb65ba69ac3
towa555 <markgolah67@.com>
2021-10-04 11:27:10 +0100

    assignment

---
+++ app.js
@@ -0,0 +1,31 @@
+
+  //=====================================
+//=====================================
+function add(a,b){
+  return a + b
+}
+function subtract(a,b){
+  return a - b
+}
+function divide(a,b){
+  return a / b
+}
+function multiply(a,b){
+  return a * b
+}
+function modulo(a,b){
+  return a % b
+}
+
+console.log(add(1,2))
+console.log(divide(10,2))
+console.log(subtract(5,2))
+console.log(multiply(2,2))
+console.log(modulo(15,10))
+//=======================================
+//=======================================
+
+
+
+
+
